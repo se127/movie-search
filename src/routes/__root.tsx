8e280091be70318933b0a631586a364f5a5990e2
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-09-11 16:25:30 +0330

    chore: update page title to 'Movie - Tv Series Search'

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Movie Search',
+        title: 'Movie - Tv Series Search',
       },
     ],
     links: [
